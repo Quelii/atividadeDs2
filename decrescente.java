b9853654b48de2bf03f3ea9fa7501f3a7824e041
Queli Gramacho <112115486+Quelii@users.noreply.github.com>
2023-02-06 19:02:38 -0300

    Add files via upload

---
+++ decrescente.java
@@ -0,0 +1,27 @@
+package atividade;
+
+import java.util.Scanner;
+
+public class decrescente {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    
+    int num, num2;
+    
+    Scanner sc = new Scanner(Systen.in);
+    System.out.println("Digite o Primeiro Número");
+    num1 = sc.nextInt();
+    
+    System.out.println("Digite o Segundo Número");
+    num2 = sc.nextInt();
+ 
+    if(num1 > num2){
+    System.out.println(num1 + ""+ num2);
+    }else{
+    System.out.println(num1 +""+num2);
+    }
+    
+    
+    }
+    }
